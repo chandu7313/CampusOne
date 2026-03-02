f5a147d17458a0433f91d5672b98a50f37e4e537
GCM <gcm@GCMs-MacBook-Air.local>
2026-03-03 00:01:34 +0530

    supabase added

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,8 +7,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     logging: false,
     dialectOptions: {
         ssl: {
-            require: true,
-            rejectUnauthorized: false // Necessary for some managed DBs like Supabase
+            rejectUnauthorized: false
         }
     },
     pool: {
